e360feb3ac37690bfd58daddb716546a9819f2e1
Rustam Garayev <rustam.garayev.std@bhos.edu.az>
2022-02-11 00:41:36 +0400

    First commit

---
+++ reading_files.sh
@@ -0,0 +1,15 @@
+#!/usr/local/bin/bash
+
+file=$1
+
+if [[ "$file" == "" || (! -f "$file") ]]
+then
+	echo Using standard input!
+	file="/dev/stdin"
+fi
+
+while read -r line
+do
+	echo "$line"
+done < "${file}"
+
